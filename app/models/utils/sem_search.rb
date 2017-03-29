7f00cc064914cd7fc202abeac3249c3155ca6755
Monty Lennie <montylennie@gmail.com>
2017-03-29 01:56:33 +0000

    finish first version of search code

--- app/models/utils/sem_search.rb
+++ app/models/utils/sem_search.rb
@@ -1,16 +1,36 @@
 require 'semantics3'
+require 'json'
 
 class SemSearch
 
-  attr_accessor :client
+  attr_accessor :client, :term, :results
 
   def initialize
     @client = Semantics3::Products.new(ENV["SEMANTIC_KEY"],ENV["SEMANTIC_SECRET"])
   end
 
-  def self.search
+  def self.search raw_term
     sem = self.new
-    binding.pry
+    sem.prepare_search raw_term
+    sem.search_and_cache
+  end
+
+  def search_and_cache
+    resultsHash = self.client.get_products()
+    self.results = resultsHash["results"]
+    self.cache_results
+  end
+
+  def prepare_search raw_term
+    self.term = raw_term.downcase.strip
+    self.client.products_field( "search", self.term )
+  end
+
+  def cache_results
+    self.results.each do |result|
+      json = result.to_json
+      SearchResult.create(json: json, term: term)
+    end
   end
 
 end
