51eaec57ecba19188a7af33ce8b4044843222107
Monty Lennie <montylennie@gmail.com>
2017-03-31 22:49:18 +0000

    add tests for SemSearch initialize and search methods

--- spec/support/sem_search_helpers.rb
+++ spec/support/sem_search_helpers.rb
@@ -1,6 +1,56 @@
 module SemSearchTestHelpers
 
-  def sem_search_initialize_sets_term_helper
-
+  class Client
+    def get_products
+      return "products"
+    end
   end
+
+  def create_sem_helper
+    key = ENV["SEMANTIC_KEY"]
+    secret = ENV["SEMANTIC_SECRET"]
+    @client = Client.new
+    allow(Semantics3::Products).to receive(:new).with(key,secret).and_return(@client)
+    SemSearch.new "iphone"
+  end
+
+  def sem_search_initialize_sets_term_helper
+    sem = create_sem_helper
+    expect(sem.term).to eq("iphone")
+  end
+
+  def sem_search_initialize_sets_client_helper
+    sem = create_sem_helper
+    expect(sem.client).to eq(@client)
+  end
+
+  def sem_search_search_calls_prepare_search_helper
+    sem = create_sem_helper
+    expect(sem).to receive(:prepare_search).once
+    sem.search
+  end
+
+  def sem_search_search_calls_search_and_cache_helper
+    sem = create_sem_helper
+    allow(sem).to receive(:prepare_search).once
+    expect(sem).to receive(:search_and_cache).once
+    sem.search
+  end
+
+  def sem_search_search_returns_results_helper
+    sem = create_sem_helper
+    allow(sem).to receive(:prepare_search).once
+    allow(sem).to receive(:search_and_cache).and_return(true)
+    sem.results = "results"
+    expect(sem.search).to eq("results")
+  end
+
+  def sem_search_search_returns_false_helper
+    sem = create_sem_helper
+    allow(sem).to receive(:prepare_search).once
+    allow(sem).to receive(:search_and_cache).and_return(false)
+    sem.results = "results"
+    expect(sem.search).to eq(false)
+  end
+
 end
--- spec/utils/sem_search_spec.rb
+++ spec/utils/sem_search_spec.rb
@@ -6,7 +6,24 @@ RSpec.describe SemSearch, type: :model do
     it "should set term property" do
      sem_search_initialize_sets_term_helper
     end
-    it "should set client property"
+    it "should set client property" do
+     sem_search_initialize_sets_client_helper
+    end
+  end
+
+  describe "search" do
+    it "calls prepare search" do
+      sem_search_search_calls_prepare_search_helper
+    end
+    it "calls search_and_cache" do
+      sem_search_search_calls_search_and_cache_helper
+    end
+    it "returns results if search_and_cache is true" do
+      sem_search_search_returns_results_helper
+    end
+    it "returns false if search_and_cache is not true" do
+      sem_search_search_returns_false_helper
+    end
   end
 end
 
